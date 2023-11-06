e43838faa53057b0bbadc75088343c9532c9aa1c
Jahangir506 <zahangirkhan506@gmail.com>
2023-11-06 15:20:15 +0600

    the firebase config is set up in the environment variable

--- src/configs/firebase.config.js
+++ src/configs/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-VoCYKO4WUhy_akh_CjnmWVhn9U3fu5M",
-  authDomain: "tong-adda.firebaseapp.com",
-  projectId: "tong-adda",
-  storageBucket: "tong-adda.appspot.com",
-  messagingSenderId: "987378898385",
-  appId: "1:987378898385:web:1567ca4d9de5d5a4adbcf0"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 
